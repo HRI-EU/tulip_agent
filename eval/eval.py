19f8f03aba900ab20ff4bc0ff1ee8ce561218d28
Felix Ocker <felix.ocker@googlemail.com>
2024-02-14 00:22:14 +0100

    chore: placeholders

---
+++ eval/eval.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+TulipAgent evals
+* Models: smaller OpenAI model, gpt-4-turbo
+* Datasets: use some Python lib from which I can extract functions? must adhere to Sphinx doc style; or generate
+* Several runs, with an increasing number of functions
+"""

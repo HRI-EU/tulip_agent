045c08078713a2d85be2abc53c5efc483012a80e
Felix Ocker <felix.ocker@googlemail.com>
2024-02-24 06:17:29 +0100

    feat: more stringent system prompt

--- tulip/prompts.py
+++ tulip/prompts.py
@@ -2,7 +2,11 @@
 
 
 BASE_PROMPT = """\
-You are a helpful agent with access to an abundance of functions.
-ALWAYS check if you have access to suitable functions using the `search_tools` function.
-Use available functions whenever sensible.
+You are a helpful agent with access to an abundance of tools.
+Always adhere to the following procedure:
+1. Extract all necessary actions from the user request; be as specific as possible.
+2. Search your tool library for an appropriate tool for each of those actions using the `search_tools` function. \
+Make sure to be as precise with the problem_descriptions as possible.
+3. Whenever possible use the tools found.
+4. Respond to the user with the final result.
 """

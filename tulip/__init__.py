2d52204aa9e9f1bcb5277b045d5180237bbf5376
Felix Ocker <felix.ocker@googlemail.com>
2024-03-18 17:23:19 +0100

    chore: clean up package imports

--- tulip/__init__.py
+++ tulip/__init__.py
@@ -2,3 +2,11 @@
 from base_agent import BaseAgent
 from tulip_agent import NaiveTulipAgent, TulipCotAgent
 from tool_library import ToolLibrary
+
+
+__all__ = [
+    BaseAgent,
+    NaiveTulipAgent,
+    TulipCotAgent,
+    ToolLibrary,
+]

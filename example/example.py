0435861885acc665572bee5edd1c5e53d18bba30
Felix Ocker <felix.ocker@googlemail.com>
2024-04-09 00:05:50 +0200

    refactor(example): dry, due to increasing number of agents

--- example/example.py
+++ example/example.py
@@ -51,54 +51,33 @@ def run_comparison():
     query = "What is 45342 * 23487 + 32478?"
     print(query)
 
-    print_seperator(name="BASE")
+    print_seperator(name=BaseAgent.__name__)
     base_agent = BaseAgent()
     base_res = base_agent.query(query)
     print(f"{base_res=}")
 
-    print_seperator(name="TOOL")
-    tool_agent = ToolAgent(functions=FUNCTIONS)
-    tool_res = tool_agent.query(query)
-    print(f"{tool_res=}")
-
-    print_seperator(name="TOOL COT")
-    tool_cot_agent = ToolCotAgent(functions=FUNCTIONS)
-    tool_cot_res = tool_cot_agent.query(query)
-    print(f"{tool_cot_res=}")
+    for agent_type in (ToolAgent, ToolCotAgent):
+        print_seperator(name=agent_type.__name__)
+        agent = agent_type(functions=FUNCTIONS)
+        res = agent.query(query)
+        print(f"{res=}")
 
     tulip = ToolLibrary(chroma_sub_dir="example/", functions=FUNCTIONS)
 
-    print_seperator(name="MINIMAL TULIP")
-    minimal_tulip_agent = MinimalTulipAgent(
-        tool_library=tulip,
-        top_k_functions=2,
+    type_k_combinations = (
+        (MinimalTulipAgent, 2),
+        (NaiveTulipAgent, 4),
+        (TulipCotAgent, 1),
+        (AutoTulipAgent, 1),
     )
-    tulip_res = minimal_tulip_agent.query(query)
-    print(f"{tulip_res=}")
-
-    print_seperator(name="NAIVE TULIP")
-    naive_tulip_agent = NaiveTulipAgent(
-        tool_library=tulip,
-        top_k_functions=4,
-    )
-    tulip_res = naive_tulip_agent.query(query)
-    print(f"{tulip_res=}")
-
-    print_seperator(name="TULIP COT")
-    tulip_cot_agent = TulipCotAgent(
-        tool_library=tulip,
-        top_k_functions=1,
-    )
-    tulip_res = tulip_cot_agent.query(query)
-    print(f"{tulip_res=}")
-
-    print_seperator(name="AUTO TULIP")
-    auto_tulip_agent = AutoTulipAgent(
-        tool_library=tulip,
-        top_k_functions=1,
-    )
-    tulip_res = auto_tulip_agent.query(query)
-    print(f"{tulip_res=}")
+    for agent_type, top_k in type_k_combinations:
+        print_seperator(name=agent_type.__name__)
+        tulip_agent = agent_type(
+            tool_library=tulip,
+            top_k_functions=top_k,
+        )
+        res = tulip_agent.query(query)
+        print(f"{res=}")
 
 
 if __name__ == "__main__":

f0ec8ba87c3b3e2e1fa18c6ab662bc4853efe949
Felix Ocker <felix.ocker@googlemail.com>
2024-04-12 13:50:57 +0200

    chore: update oai model version

--- tulip_agent/constants.py
+++ tulip_agent/constants.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 BASE_EMBEDDING_MODEL = "text-embedding-ada-002"
-BASE_LANGUAGE_MODEL = "gpt-4-0125-preview"
+BASE_LANGUAGE_MODEL = "gpt-4-turbo-2024-04-09"
 BASE_TEMPERATURE = 0.0

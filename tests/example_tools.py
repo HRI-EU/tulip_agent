44a944c509ee06bab917f1b48878a302be16c287
Felix Ocker <felix.ocker@googlemail.com>
2024-04-18 22:04:22 +0200

    test: update tool library tests

--- tests/example_tools.py
+++ tests/example_tools.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
 
+def add(a: float, b: float) -> float:
+    """
+    Add two numbers.
+
+    :param a: The first number.
+    :param b: The second number.
+    :return: The sum of a and b.
+    """
+    return a + b
+
+
+def subtract(a: float, b: float) -> float:
+    """
+    Subtract two numbers.
+
+    :param a: The number to be subtracted from.
+    :param b: The number to subtract.
+    :return: The difference of a and b.
+    """
+    return a - b
+
+
 def multiply(a: float, b: float) -> float:
     """
     Multiply two numbers.
--- tests/test_tool_library.py
+++ tests/test_tool_library.py
@@ -4,68 +4,51 @@ import unittest
 from tulip_agent.tool_library import ToolLibrary
 
 
-def add(a: float, b: float) -> float:
-    """
-    Add two numbers.
-
-    :param a: The first number.
-    :param b: The second number.
-    :return: The sum of a and b.
-    """
-    return a + b
-
-
-def subtract(a: float, b: float) -> float:
-    """
-    Subtract two numbers.
-
-    :param a: The number to be subtracted from.
-    :param b: The number to subtract.
-    :return: The difference of a and b.
-    """
-    return a - b
-
-
 class TestCore(unittest.TestCase):
     def setUp(self):
         tulip = ToolLibrary(chroma_sub_dir="test/")
         tulip.chroma_client.delete_collection("tulip")
 
     def test_init(self):
-        tulip = ToolLibrary(chroma_sub_dir="test/", functions=[add, subtract])
-        functions = tulip.collection.get(include=[])["ids"]
-        self.assertEqual(
-            set(functions),
-            {"add", "subtract"},
-            "Initializing tool library with functions failed.",
-        )
-
-    def test_search_function(self):
-        tulip = ToolLibrary(chroma_sub_dir="test/", functions=[add, subtract])
-        res = tulip.search(problem_description="add 4 and 5", top_k=1)
-        self.assertEqual(res["ids"][0][0], "add", "Searching for function failed.")
-
-    def test_add_function(self):
-        tulip = ToolLibrary(chroma_sub_dir="test/", functions=[subtract])
-        tulip.add_function(function=add)
-        functions = tulip.collection.get(include=[])["ids"]
-        self.assertEqual(set(functions), {"add", "subtract"}, "Adding function failed.")
-
-    def test_remove_function(self):
-        tulip = ToolLibrary(chroma_sub_dir="test/", functions=[add, subtract])
-        tulip.remove_function(function_name="add")
-        functions = tulip.collection.get(include=[])["ids"]
-        self.assertEqual(set(functions), {"subtract"}, "Removing function failed.")
-
-    def test_load_all_from_file(self):
         tulip = ToolLibrary(
             chroma_sub_dir="test/", file_imports=[("example_tools", [])]
         )
         functions = tulip.collection.get(include=[])["ids"]
         self.assertEqual(
             set(functions),
-            {"example_tools.divide", "example_tools.multiply"},
-            "Loading all functions from file failed.",
+            {
+                "example_tools__add",
+                "example_tools__subtract",
+                "example_tools__multiply",
+                "example_tools__divide",
+            },
+            "Initializing tool library with functions failed.",
+        )
+
+    def test_init_specific(self):
+        tulip = ToolLibrary(
+            chroma_sub_dir="test/", file_imports=[("example_tools", ["add"])]
+        )
+        functions = tulip.collection.get(include=[])["ids"]
+        self.assertEqual(
+            set(functions),
+            {"example_tools__add"},
+            "Initializing tool library with selected functions failed.",
+        )
+
+    def test_load_file(self):
+        tulip = ToolLibrary(chroma_sub_dir="test/")
+        tulip.load_functions_from_file(module_name="example_tools", function_names=[])
+        functions = tulip.collection.get(include=[])["ids"]
+        self.assertEqual(
+            set(functions),
+            {
+                "example_tools__add",
+                "example_tools__subtract",
+                "example_tools__multiply",
+                "example_tools__divide",
+            },
+            "Loading entire file failed.",
         )
 
     def test_load_names_from_file(self):
@@ -75,20 +58,33 @@ class TestCore(unittest.TestCase):
         functions = tulip.collection.get(include=[])["ids"]
         self.assertEqual(
             set(functions),
-            {"example_tools.multiply"},
+            {"example_tools__multiply"},
             "Loading selected functions from file failed.",
         )
 
-    def test_load_functions_from_file(self):
-        tulip = ToolLibrary(chroma_sub_dir="test/")
-        tulip.load_functions_from_file(
-            modulename="example_tools", function_names=["multiply"]
+    def test_search_function(self):
+        tulip = ToolLibrary(
+            chroma_sub_dir="test/", file_imports=[("example_tools", [])]
         )
+        res = tulip.search(problem_description="add 4 and 5", top_k=1)
+        self.assertEqual(
+            res["ids"][0][0], "example_tools__add", "Searching for function failed."
+        )
+
+    def test_remove_function(self):
+        tulip = ToolLibrary(
+            chroma_sub_dir="test/", file_imports=[("example_tools", [])]
+        )
+        tulip.remove_function(function_id="example_tools__add")
         functions = tulip.collection.get(include=[])["ids"]
         self.assertEqual(
             set(functions),
-            {"example_tools.multiply"},
-            "Loading selected functions from file failed.",
+            {
+                "example_tools__subtract",
+                "example_tools__multiply",
+                "example_tools__divide",
+            },
+            "Removing function failed.",
         )
 
     def test_execute(self):
@@ -96,7 +92,7 @@ class TestCore(unittest.TestCase):
             chroma_sub_dir="test/", file_imports=[("example_tools", ["multiply"])]
         )
         res = tulip.execute(
-            function_name="example_tools.multiply", function_args={"a": 2.0, "b": 2.0}
+            function_id="example_tools__multiply", function_args={"a": 2.0, "b": 2.0}
         )
         self.assertEqual(
             res,

980ac5f74ff2e84481d92029949f4467433f2d7e
Felix Ocker <felix.ocker@honda-ri.de>
2024-05-21 17:41:43 +0200

    feat(tulip_agent): revise cot prompt

--- src/tulip_agent/prompts.py
+++ src/tulip_agent/prompts.py
@@ -53,11 +53,11 @@ Obey the following rules:
 TULIP_COT_PROMPT = """\
 You are a helpful agent who has access to an abundance of tools.
 Always adhere to the following procedure:
-1. Break the user request down into atomic actions.
-2. Search your tool library for appropriate tools for these atomic actions using the `search_tools` function. \
-Provide generic action descriptions to ensure that you find generic tools.
-3. Whenever possible use the tools found to fulfill the user request.
-4. Respond to the user with the final result.
+1. Break the user request down into atomic tasks.
+2. Search your tool library for appropriate tools for these atomic tasks using the `search_tools` function. \
+Provide generic task descriptions to ensure that you find generic tools.
+3. Whenever possible use the tools found to solve the atomic tasks.
+4. Respond to the user with the final result, never with an intermediate result.
 """
 
 

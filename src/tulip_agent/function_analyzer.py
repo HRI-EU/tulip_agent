e33fdc6f09d712674b9162d1dc60cd5600baabf9
Felix Ocker <felix.ocker@honda-ri.de>
2024-08-08 22:41:27 +0200

    refactor(function_analyzer): use pydantic for type introspection

--- src/tulip_agent/function_analyzer.py
+++ src/tulip_agent/function_analyzer.py
@@ -31,62 +31,10 @@
 Analysis of Python functions and entire classes using introspection
 for creating descriptions usable with the OpenAI API
 
-Note: Only OPENAI_TYPES are supported as function inputs
+Note: Function calling w structured outputs is limited to a subset of the JSON schema language
+https://platform.openai.com/docs/guides/function-calling
 """
-import inspect
-import typing
-from dataclasses import dataclass
-
-
-OPENAI_BASE_TYPES = {
-    float: "number",
-    int: "number",
-    str: "string",
-    bool: "boolean",
-}
-
-OPENAI_NESTED_TYPES = {
-    list: "array",
-    tuple: "array",
-    set: "array",
-}
-
-
-@dataclass
-class VariableDescription:
-    name: str
-    type_: type
-    description: str
-
-    def __post_init__(self):
-        # NOTE: assumes simple type hint structure; uses first type if there are distinct ones
-        def _recurse(type_):
-            if type_ in OPENAI_BASE_TYPES:
-                return [OPENAI_BASE_TYPES[type_]]
-            elif typing.get_origin(type_) in OPENAI_NESTED_TYPES:
-                type_origin = OPENAI_NESTED_TYPES[typing.get_origin(type_)]
-                type_args = _recurse(typing.get_args(type_)[0])
-                return [type_origin] + type_args
-            else:
-                raise TypeError(
-                    f"Unexpected subtype for {self.name}: {str(self.type_)}."
-                )
-
-        self.type_structure = _recurse(type_=self.type_)
-
-    def to_dict(self) -> dict:
-        base_result = {
-            self.name: {
-                "type": self.type_structure[0],
-                "description": self.description,
-            }
-        }
-        if len(self.type_structure) > 1:
-            sub = base_result[self.name]
-            for t in self.type_structure[1:]:
-                sub["items"] = {"type": t}
-                sub = sub["items"]
-        return base_result
+import pydantic
 
 
 class FunctionAnalyzer:
@@ -98,72 +46,34 @@ class FunctionAnalyzer:
         Assumptions:
         * docstring includes a function description and parameter descriptions separated by 2 linebreaks
         * docstring includes parameter descriptions indicated by :param x:
-        NOTE:
-        * for now, only simple file types and lists thereof are supported
-        * inputs may be optional
-        * inputs that are unions default to the first type; [int, str] -> int (which is interpreted as number)
         """
         name = function_.__name__
 
         # analyze type hints
-        signature = inspect.signature(function_)
-        type_hints = typing.get_type_hints(function_)
-        type_hints.pop("return", None)
-        required = [
-            th
-            for th in type_hints
-            if not (
-                typing.get_origin(type_hints[th]) is typing.Optional
-                or typing.get_origin(type_hints[th]) is typing.Union
-                and type(None) in typing.get_args(type_hints[th])
-            )
-            and signature.parameters.get(th).default is inspect.Parameter.empty
-        ]
-        type_hints_basic = {
-            k: (
-                v
-                if typing.get_origin(v) is not typing.Union
-                else (
-                    typing.get_args(v)[0]
-                    if k in required
-                    else [t for t in typing.get_args(type_hints[k]) if t][0]
-                )
-            )
-            for k, v in type_hints.items()
-        }
+        parameters = pydantic.TypeAdapter(function_).json_schema()
 
         # analyze doc string
         descriptions = [e.strip() for e in function_.__doc__.split(":param ")]
-        function_description, param_descriptions = descriptions[0], descriptions[1:]
-        param_descriptions = {
+        function_description, parameter_descriptions = descriptions[0], descriptions[1:]
+        parameter_descriptions = {
             k: v
             for (k, v) in [
                 e.split(":return:")[0].strip().split(": ")
-                for e in param_descriptions
+                for e in parameter_descriptions
                 if e
             ]
         }
-        variable_descriptions = [
-            VariableDescription(
-                name=v,
-                type_=type_hints_basic[v],
-                description=param_descriptions[v],
-            ).to_dict()
-            for v in type_hints_basic
-        ]
-        properties = {k: v for d in variable_descriptions for k, v in d.items()}
+        for parameter, parameter_description in parameter_descriptions.items():
+            parameters["properties"][parameter]["description"] = parameter_description
 
         return {
             "type": "function",
             "function": {
                 "name": name,
                 "description": function_description,
-                "parameters": {
-                    "type": "object",
-                    "properties": properties,
-                    "required": required,
-                },
+                "parameters": parameters,
             },
+            "strict": True,
         }
 
     def analyze_class(self, class_: object) -> list:

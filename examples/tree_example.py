e8834121037ddc1c171c8ce2062c0d1e7fb2e454
Felix Ocker <felix.ocker@honda-ri.de>
2024-08-09 00:34:05 +0200

    docs: add examples

--- examples/tree_example.py
+++ examples/tree_example.py
@@ -38,11 +38,17 @@ logging.basicConfig(level=logging.INFO)
 tasks = [
     # """Add 2 and 5""",
     # """Add the product of 3 and 4 and the product of 5 and 6""",
-    """Calculate the square root of the sum of the product of 3 and 4 and the product of 5 and 6""",
+    # """Calculate the square root of the sum of the product of 3 and 4 and the product of 5 and 6""",
+    """Take an image of the table and convert that to a jpg""",
 ]
 
 tulip = ToolLibrary(chroma_sub_dir="example/", file_imports=[("calculator", [])])
-agent = TreeTulipAgent(tool_library=tulip, top_k_functions=3)
+agent = TreeTulipAgent(
+    tool_library=tulip,
+    top_k_functions=3,
+    search_similarity_threshold=1.25,
+    max_recursion_depth=2,
+)
 
 for task in tasks:
     print(f"{task=}")

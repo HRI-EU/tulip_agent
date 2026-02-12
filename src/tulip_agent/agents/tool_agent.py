fab7a942f03b8c0089dd9729c9cc3a0f157ab7f9
Felix Ocker <felix.ocker@honda-ri.de>
2026-02-12 18:20:52 +0100

    feat: parallel tool exec

--- src/tulip_agent/agents/tool_agent.py
+++ src/tulip_agent/agents/tool_agent.py
@@ -35,7 +35,6 @@
 """
 Tool agent ABC.
 """
-import concurrent.futures
 import json
 import logging
 from abc import ABC
@@ -45,6 +44,8 @@ from openai import AzureOpenAI, OpenAI
 
 from tulip_agent.agents.base_agent import LlmAgent
 from tulip_agent.function_analyzer import FunctionAnalyzer
+from tulip_agent.tool import InternalTool
+from tulip_agent.tool_execution import Job, execute_tool_calls
 
 
 logger = logging.getLogger(__name__)
@@ -72,15 +73,22 @@ class ToolAgent(LlmAgent, ABC):
             api_interaction_limit=api_interaction_limit,
         )
         self.function_analyzer = FunctionAnalyzer()
-        functions_ = functions + [self.stop]
-        self.tools = {f.__name__: f for f in functions_}
-        self.tool_descriptions = [
-            self.function_analyzer.analyze_function(f) for f in functions_
-        ]
-        self.tool_timeout: int = 60
-        self.tool_timeout_message: str = (
-            "Error: The tool did not return a response within the specified timeout."
+        stop_tool = InternalTool(
+            function_name=self.stop.__name__,
+            definition=self.function_analyzer.analyze_function(self.stop),
+            function=self.stop,
         )
+        self.tools = {
+            function.__name__: InternalTool(
+                function_name=function.__name__,
+                definition=self.function_analyzer.analyze_function(function),
+                function=function,
+                timeout=60,
+                timeout_message="Error: The tool did not return a response within the specified timeout.",
+            )
+            for function in functions
+        }
+        self.tools["stop_tool"] = stop_tool
         self.response: str | None = None
 
     def stop(self, message: str) -> str:
@@ -98,7 +106,7 @@ class ToolAgent(LlmAgent, ABC):
         self.response = None
         response = self._get_response(
             msgs=self.messages,
-            tools=self.tool_descriptions,
+            tools=[tool.definition for tool in self.tools],
             tool_choice="required",
         )
         response_message = response.choices[0].message
@@ -121,58 +129,84 @@ class ToolAgent(LlmAgent, ABC):
                 )
                 return error_message
 
-            for tool_call in tool_calls:
-                func_name = tool_call.function.name
+            tool_messages = [{} for _ in tool_calls]
+            valid_calls = []
+            jobs = []
 
-                with concurrent.futures.ThreadPoolExecutor() as executor:
-                    try:
-                        func_args = json.loads(tool_call.function.arguments)
-                        future = executor.submit(self.tools[func_name], **func_args)
-                        function_response = future.result(timeout=self.tool_timeout)
-                    except json.decoder.JSONDecodeError as e:
-                        logger.error(e)
-                        generated_func_name = func_name
-                        func_name = "invalid_tool_call"
-                        tool_call.function.name = func_name
-                        tool_call.function.arguments = "{}"
-                        function_response = f"Error: Invalid arguments for {func_name} (previously {generated_func_name}): {e}"
-                    except KeyError as e:
-                        logger.error(
-                            f"Invalid tool `{func_name}` resulting in error: {e}"
-                        )
-                        generated_func_name = func_name
-                        func_name = "invalid_tool_call"
-                        tool_call.function.name = func_name
-                        tool_call.function.arguments = "{}"
-                        function_response = f"Error: {generated_func_name} is not a valid tool. Use only the tools available."
-                    except concurrent.futures.TimeoutError as e:
-                        logger.error(
-                            f"{type(e).__name__}: {func_name} did not return a result before timeout."
-                        )
-                        function_response = self.tool_timeout_message
-                    except Exception as e:
-                        logger.error(e)
-                        function_response = (
-                            f"Error: Invalid tool call for {func_name}: {e}"
-                        )
-                self.messages.append(
-                    {
+            for i, tool_call in enumerate(tool_calls):
+                func_name = tool_call.function.name
+                try:
+                    func_args = json.loads(tool_call.function.arguments)
+                except json.decoder.JSONDecodeError as e:
+                    logger.error(e)
+                    generated_func_name, func_name = func_name, "invalid_tool_call"
+                    tool_call.function.name = func_name
+                    tool_call.function.arguments = "{}"
+                    function_response = f"Error: Invalid arguments for {func_name} (previously {generated_func_name}): {e}"
+                    tool_messages[i] = {
                         "tool_call_id": tool_call.id,
                         "role": "tool",
                         "name": func_name,
-                        "content": str(function_response),
+                        "content": function_response,
                     }
+                    continue
+
+                if func_name not in self.tools:
+                    logger.error(f"Invalid tool `{func_name}`.")
+                    generated_func_name = func_name
+                    func_name = "invalid_tool_call"
+                    tool_call.function.name = func_name
+                    tool_call.function.arguments = "{}"
+                    function_response = f"Error: {generated_func_name} is not a valid tool. Use only the tools available."
+                    tool_messages[i] = {
+                        "tool_call_id": tool_call.id,
+                        "role": "tool",
+                        "name": func_name,
+                        "content": function_response,
+                    }
+                    continue
+
+                valid_calls.append((i, tool_call, func_name))
+                jobs.append(
+                    Job(
+                        tool_call_id=tool_call.id,
+                        tool=self.tools[func_name],
+                        parameters=func_args,
+                    )
                 )
+
+            execution_results = execute_tool_calls(jobs=jobs)
+            for (i, tool_call, func_name), execution_result in zip(
+                valid_calls, execution_results
+            ):
+                if execution_result.result.error:
+                    logger.error(execution_result.result.error)
+                    function_response = execution_result.result.error
+                    func_name = "invalid_tool_call"
+                    tool_call.function.name = func_name
+                    tool_call.function.arguments = "{}"
+                else:
+                    function_response = execution_result.result.value
+
+                tool_messages[i] = {
+                    "tool_call_id": tool_call.id,
+                    "role": "tool",
+                    "name": func_name,
+                    "content": function_response,
+                }
+
+            for tool_message, tool_call in zip(tool_messages, tool_calls):
+                self.messages.append(tool_message)
                 logger.info(
                     (
-                        f"Function {func_name} returned `{str(function_response)}` "
+                        f"Function {tool_message['name']} returned `{tool_message['content']}` "
                         f"for arguments {tool_call.function.arguments}."
                     )
                 )
 
             response = self._get_response(
                 msgs=self.messages,
-                tools=self.tool_descriptions,
+                tools=[tool.definition for tool in self.tools],
                 tool_choice="required",
             )
             response_message = response.choices[0].message
